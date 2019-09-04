dfb125c61bc7b188f9a38acc1f34e6ac082d44c8
parvez <parvez84@gmail.com>
2019-09-04 21:05:31 +0000

    more changes

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "hashicorp-workshop"
+    organization = "pazim-sandbox"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-azure"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -60,16 +60,16 @@ variable "admin_password" {
 }
 
 variable "height" {
-  default     = "400"
+  default     = "600"
   description = "Image height in pixels."
 }
 
 variable "width" {
-  default     = "600"
+  default     = "800"
   description = "Image width in pixels."
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "placebeard.it"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
